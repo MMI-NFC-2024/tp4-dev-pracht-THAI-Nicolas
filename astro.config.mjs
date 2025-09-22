1285dae9214753f6ef4f3021504b8b2ee7d236d3
THAI-Nicolas <nicolas.thai@edu.univ-fcomte.fr>
2025-09-22 17:27:05 +0200

    Amélioration de l'esthéthique graphique et importation de composants pour certains endroits

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  adapter: netlify()
+  integrations: [tailwind()],
+  adapter: netlify(),
 });
